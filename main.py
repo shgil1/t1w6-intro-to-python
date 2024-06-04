3369ab5ac555a00e35deae9da935191d5bd7162e
shgil1 <15648@coderacademy.edu.au>
2024-06-04 20:29:26 +1000

    data types

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
-print("Hello world!")
-print(5-3)
-print("Don't forget to commit!")
+# print("Hello world!")
 
+
+#floats
+# 2.5
+# 5.123123
+# print(3.1414)
+# print(type(-5.63))
+# print(type(-5))
+
+#complex (imaginary numbers), no worries, we won't cover these kind of numbers in the course
+# 2 + 3j
+# print(type(4 + 7j))
+
+#operators + - * / %
+# print(4 * 5)
+# print(15 /3 )
+# print(15 / 2)
+# print(7 % 2)
+
+#String
+# "Hello world"
+# 'Hello'
+# print("This is a string")
+
+#String concentrations, addition, join
+# print("Hello" + "World")
+# print(3 +5)
+# print("3" + "5")
+
+#boolean
+# True
+# False
+# print(type(False))
+0 # falsy value 
+## # falsy value 
+# print(bool(0))
+# print(bool(""))
+# print(bool(-123))
+# print(bool("False"))
+
+#comparison operators <, >, <=, >=, ==, !=
+# print (2 < 3)
+# print(5 >= 5)
+# print("Hello".lower () == "hello")
+
+#logical operators and, or, not
+print (True and False) # AND returns True when both operators are True
+print (False or True) # OR returns True, when one or both of the operators are True
+print(not False) # NOT returns the opposite value
